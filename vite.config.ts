64b5eb748b1c5d4f0ddcfa762946892f2af67e25
ishansaxena012 <06ishansaxena@gmail.com>
2025-07-28 02:49:47 +0530

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: mode === 'production' ? '/is-portfolio/' : '/',
+  base: '/', 
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
-    viteStaticCopy({
-      targets: [
-        { src: 'dist/index.html', dest: '', rename: '404.html' }
-      ]
-    })
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
